a35b9d28f0085b468b495e107464aac3ba628437
Jesus Gomez <jagomez21@miners.utep.edu>
2018-02-28 15:26:09 -0700

    methods numberClicked and boardClicked now record values.

--- 2D Sudoku/src/sudoku/dialog/BoardPanel.java
+++ 2D Sudoku/src/sudoku/dialog/BoardPanel.java
@@ -20,9 +20,9 @@ import sudoku.model.Board;
  */
 @SuppressWarnings("serial")
 public class BoardPanel extends JPanel {
-    
+
 	public interface ClickListener {
-		
+
 		/** Callback to notify clicking of a square. 
 		 * 
 		 * @param x 0-based column index of the clicked square
@@ -30,66 +30,67 @@ public class BoardPanel extends JPanel {
 		 */
 		void clicked(int x, int y);
 	}
-	
-    /** Background color of the board. */
-	private static final Color boardColor = new Color(247, 223, 150);
 
-    /** Board to be displayed. */
-    private Board board;
+	/** Background color of the board. */
+	private static final Color boardColor = new Color(112, 163, 252);
 
-    /** Width and height of a square in pixels. */
-    private int squareSize;
+	/** Board to be displayed. */
+	private Board board;
 
-    /** Create a new board panel to display the given board. */
-    public BoardPanel(Board board, ClickListener listener) {
-        this.board = board;
-        addMouseListener(new MouseAdapter() {
-            public void mouseClicked(MouseEvent e) {
-            	int xy = locateSquaree(e.getX(), e.getY());
-            	if (xy >= 0) {
-            		listener.clicked(xy / 100, xy % 100);
-            	}
-            }
-        });
-    }
+	/** Width and height of a square in pixels. */
+	private int squareSize;
 
-    /** Set the board to be displayed. */
-    public void setBoard(Board board) {
-    	this.board = board;
-    }
-    
-    /**
-     * Given a screen coordinate, return the indexes of the corresponding square
-     * or -1 if there is no square.
-     * The indexes are encoded and returned as x*100 + y, 
-     * where x and y are 0-based column/row indexes.
-     */
-    private int locateSquaree(int x, int y) {
-    	if (x < 0 || x > board.size * squareSize
-    			|| y < 0 || y > board.size * squareSize) {
-    		return -1;
-    	}
-    	int xx = x / squareSize;
-    	int yy = y / squareSize;
-    	return xx * 100 + yy;
-    }
+	/** Create a new board panel to display the given board. */
+	public BoardPanel(Board board, ClickListener listener) {
+		this.board = board;
+		addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				int xy = locateSquaree(e.getX(), e.getY());
+				if (xy >= 0) {
+					listener.clicked(xy / 100, xy % 100);
+				}
+			}
+		});
+	}
 
-    /** Draw the associated board. */
-    @Override
-    public void paint(Graphics g) {
-        super.paint(g); 
+	/** Set the board to be displayed. */
+	public void setBoard(Board board) {
+		this.board = board;
+	}
 
-        // determine the square size
-        Dimension dim = getSize();
-        squareSize = Math.min(dim.width, dim.height) / board.size;
+	/**
+	 * Given a screen coordinate, return the indexes of the corresponding square
+	 * or -1 if there is no square.
+	 * The indexes are encoded and returned as x*100 + y, 
+	 * where x and y are 0-based column/row indexes.
+	 */
+	private int locateSquaree(int x, int y) {
+		if (x < 0 || x > board.size * squareSize
+				|| y < 0 || y > board.size * squareSize) {
+			return -1;
+		}
+		int xx = x / squareSize;
+		int yy = y / squareSize;
+		return xx * 100 + yy;
+	}
 
-        // draw background
-        final Color oldColor = g.getColor();
-        g.setColor(boardColor);
-        g.fillRect(0, 0, squareSize * board.size, squareSize * board.size);
+	/** Draw the associated board. */
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g); 
 
-        // WRITE YOUR CODE HERE ...
-        // i.e., draw grid and squares.
-    }
+		// determine the square size
+		Dimension dim = getSize();
+		squareSize = Math.min(dim.width, dim.height) / board.size;
+
+		// draw background
+		final Color oldColor = g.getColor();
+		g.setColor(boardColor);
+		g.fillRect(0, 0, squareSize * board.size, squareSize * board.size);
+
+		// WRITE YOUR CODE HERE ...
+		// i.e., draw grid and squares.
+		
+	}
 
 }
