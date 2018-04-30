f75098057b678a6614d8f9689672eccc760e03c9
Jesus Gomez <jagomez21@miners.utep.edu>
2018-04-30 13:39:02 -0600

    Partial pairAsClient method (Not working)

--- 2D Sudoku/src/sudoku/hw5/HW5Main.java
+++ 2D Sudoku/src/sudoku/hw5/HW5Main.java
@@ -1,7 +1,9 @@
 package sudoku.hw5;
 
 import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 import javax.swing.ImageIcon;
@@ -15,74 +17,84 @@ import sudoku.model.Board;
 
 public class HW5Main extends SudokuDialog implements MessageListener{
 
-	
+
 	public HW5Main() {
 		super();
 	}
-	
+
 	public static void main(String[] args) {
 		new HW5Main();
 	}
-	
+
 	private ImageIcon NETWORK_OFF, NETWORK_ON;
 	private JButton networkButton;
 
 	protected JToolBar initToolBar() {
-	  JToolBar toolBar = super.initToolBar();
-	  NETWORK_OFF = createImageIcon("wifi-red.png");
-	  networkButton = new JButton(NETWORK_OFF);
-	  networkButton.addActionListener(this::networkButtonClicked);
-	  networkButton.setToolTipText("Pair");
-	  networkButton.setFocusPainted(false);
-	  toolBar.add(networkButton, toolBar.getComponentCount() - 1);
-	  return toolBar;
+		JToolBar toolBar = super.initToolBar();
+		NETWORK_OFF = createImageIcon("wifi-red.png");
+		networkButton = new JButton(NETWORK_OFF);
+		networkButton.addActionListener(this::networkButtonClicked);
+		networkButton.setToolTipText("Pair");
+		networkButton.setFocusPainted(false);
+		toolBar.add(networkButton, toolBar.getComponentCount() - 1);
+		return toolBar;
 	}
-	
-	 private void networkButtonClicked(ActionEvent e) { 
-		 new Thread(()-> {
-			 try {
-				 Socket socket = new Socket();
-				 socket.connect(new InetSocketAddress("127.0.0.1", 8000), 5000); // timeout in millis
-				 pairAsClient(socket);
-			 } catch (Exception e1) { }
-		 }).start();
-	 }
 
-	 private void pairAsClient(Socket socket) {
-		 network = new NetworkAdapter(socket);
-		 network.setMessageListener(this); // see the next slide
-		 network.writeJoin();
-		 network.receiveMessages(); // loop till disconnected
-	 }
+	private void networkButtonClicked(ActionEvent e) { 
+		new Thread(()-> {
+			try {
+				Socket socket = new Socket();
+				socket.connect(new InetSocketAddress("127.0.0.1", 8000), 5000); // timeout in millis
+				pairAsServer(socket);
+			} catch (Exception e1) { }
+		}).start();
+	}
 
-	 private void pairAsServerr(Socket socket) {
-		 
-	 }
-	 
-	 private NetworkAdapter network;
-	 
-	 protected void fillNumber(int x, int y, int n) {
-		 board.insert(x,y,n);
-		 if (network != null) { network.writeFill(x, y, n); } 
-	 }	
+	private void pairAsClient(Socket socket) {
+		network = new NetworkAdapter(socket);
+		network.setMessageListener(this); // see the next slide
+		network.writeJoin();
+		network.receiveMessages(); // loop till disconnected
+	}
 
-	 /** Called when a message is received from the peer. */
-	 public void messageReceived(MessageType type, int x, int y, int z, int[] others) {
-		 System.out.println(type.toString());
-		 switch (type) {
-		 case FILL:
-			 // peer filled the square (x, y) with the number z
-			 board.insert(x, y, z);
-			 break;
-		 case JOIN_ACK:
-			 board = new Board(y,others);
-			 //boardPanel.setBoard(board);
-			 break;
-		 case NEW:
-			 board = new Board(x,others);
-			 //boardPanel.setBoard(board);
-			 break;
-		 }
-		 boardPanel.repaint();
-	 }
+	private void pairAsServer(Socket socket) {
+		network = new NetworkAdapter(socket);
+		try {
+			ServerSocket server = new ServerSocket(socket.getPort()); //server socket
+			while(true) {
+				Socket client = server.accept(); //wait for connection
+				//sudoku
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	private NetworkAdapter network;
+
+	protected void fillNumber(int x, int y, int n) {
+		board.insert(x,y,n);
+		if (network != null) { network.writeFill(x, y, n); } 
+	}	
+
+	/** Called when a message is received from the peer. */
+	public void messageReceived(MessageType type, int x, int y, int z, int[] others) {
+		System.out.println(type.toString());
+		switch (type) {
+		case FILL:
+			// peer filled the square (x, y) with the number z
+			board.insert(x, y, z);
+			break;
+		case JOIN_ACK:
+			board = new Board(y,others);
+			//boardPanel.setBoard(board);
+			break;
+		case NEW:
+			board = new Board(x,others);
+			//boardPanel.setBoard(board);
+			break;
+		}
+		boardPanel.repaint();
+	}
 }
